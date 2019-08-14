722e79a10286816e511b13d03b4235d05d93a40f
ningdong <344805262@qq.com>
2019-08-14 22:12:55 +0800

    springmvc

--- HelloSpringMVC/src/controller/HelloController.java
+++ HelloSpringMVC/src/controller/HelloController.java
@@ -1,13 +1,15 @@
 package controller;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
-public class HelloController implements Controller {
+@Controller
+public class HelloController {
 
+    @RequestMapping("/hello")
     public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
         ModelAndView mav = new ModelAndView("index.jsp");
         mav.addObject("message", "Hello Spring MVC");
         return mav;
     }
+
 }
