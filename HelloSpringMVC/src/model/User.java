86ffd72e46799770c782a2ac08c717971b51eead
ningdong <344805262@qq.com>
2019-08-19 22:35:42 +0800

    完工啦

---
+++ HelloSpringMVC/src/model/User.java
@@ -0,0 +1,22 @@
+package model;
+
+public class User {
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    String userName;
+    String password;
+}
