9d134d11bb28ee757cbb6538070e3384c3a7d32b
ningdong <344805262@qq.com>
2019-08-26 23:34:52 +0800

    提交

---
+++ HelloSpringMVC/out/artifacts/HelloSpringMVC_war_exploded/WEB-INF/page/test.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 秦飞飞
+  Date: 2019/8/26
+  Time: 23:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>test</title>
+</head>
+<body>
+test
+</body>
+</html>
--- HelloSpringMVC/src/controller/HelloController.java
+++ HelloSpringMVC/src/controller/HelloController.java
@@ -1,51 +1,25 @@
 package controller;
-import com.sun.org.apache.bcel.internal.generic.RETURN;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Controller;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import model.User;
-
-@org.springframework.stereotype.Controller
-// @RequestMapping("/wmyskxz")
+@Controller
+//@RequestMapping("/mvc")
 public class HelloController {
+//@Override
+//public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
+//    ModelAndView mav = new ModelAndView("index.jsp");
+//    mav.addObject("message", "Hello Spring MVC");
+//    return mav;
+//}
 
-    @RequestMapping("/hello")
-    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-        ModelAndView mav = new ModelAndView("index");
-        mav.addObject("message", "Hello Spring MVC");
-        return mav;
-    }
-
-    @RequestMapping("/test")
-    public ModelAndView testPage(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return new ModelAndView("test");
-    }
-
-    @RequestMapping("/param")
-    public ModelAndView getParam(User user) {
-        System.out.println(user.getUserName());
-        System.out.println(user.getPassword());
-        return null;
-    }
-
-    @ModelAttribute
-    public void Model(Model model) {
-        model.addAttribute("message", "成功");
-    }
-
-    @RequestMapping("/value")
-    public String showValue() {
-        return "test2";
-    }
-
-    @RequestMapping("/jump")
-    public ModelAndView jump() {
-        ModelAndView mav = new ModelAndView("redirect:/hello");
-        return mav;
-    }
+@RequestMapping("hello")
+public ModelAndView helloRequest(HttpServletRequest request, HttpServletRequest response) {
+    ModelAndView mav = new ModelAndView("index");
+    mav.addObject("message", "hello world mvc!");
+    return mav;
+}
 }
