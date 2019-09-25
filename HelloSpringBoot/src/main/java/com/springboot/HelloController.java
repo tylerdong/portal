7bb8d8765514ade35550127642930a51735ff29e
ningdong <894795786@qq.com>
2019-09-25 20:02:57 +0800

    HelloSpringMVC

---
+++ HelloSpringBoot/src/main/java/com/springboot/HelloController.java
@@ -0,0 +1,31 @@
+package com.springboot;
+
+//import javafx.scene.input.DataFormat;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+import java.text.DateFormat;
+//import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+@Controller
+public class HelloController {
+    //@Value("${name}")
+    //private String name;
+    //
+    //@Value("${age}")
+    //private String age;
+
+    //@Autowired
+    //private StudentProperties studentProperties;
+
+    @RequestMapping("/hello")
+    public String hello(Model m) {
+        m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
+        return "hello";
+    }
+}
