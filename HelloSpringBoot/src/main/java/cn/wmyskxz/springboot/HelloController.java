68df1ecca564122d66991d4e27b7c39cc710915d
ningdong <894795786@qq.com>
2019-10-14 19:44:59 +0800

    修改bug

--- HelloSpringBoot/src/main/java/cn/wmyskxz/springboot/HelloController.java
+++ HelloSpringBoot/src/main/java/cn/wmyskxz/springboot/HelloController.java
@@ -1,18 +1,18 @@
 package cn.wmyskxz.springboot;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.text.DateFormat;
 import java.util.Date;
 //import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
     @RequestMapping("/hello")
     public String hello(Model model) {
         model.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
-        return "hello";
+        return "helloa";
     }
 }
--- HelloSpringBoot/src/main/java/cn/wmyskxz/springboot/SpringbootApplication.java
+++ HelloSpringBoot/src/main/java/cn/wmyskxz/springboot/SpringbootApplication.java
@@ -2,8 +2,10 @@ package cn.wmyskxz.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "cn.wmyskxz.springboot")
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
